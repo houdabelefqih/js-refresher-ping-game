88c08acb6128b7622c57b48618364deeba7098b4
Houda Belefqih <houdabelefqih@gmail.com>
2020-05-17 05:05:30 +0000

    added event listerners + game logic

---
+++ app.js
@@ -0,0 +1,73 @@
+/*
+ * Ping game
+ */
+
+// variable declarations and initialization
+var scores, roundScore, currentPlayer;
+
+// counters are set to 0 for both players
+resetCounters()
+
+// handle a new game 
+document.querySelector(".btn-new").addEventListener("click", resetCounters);
+
+// handle the rolling of the dice
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // display the appropriate dice image
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "images/dice-" + dice + ".png";
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.getElementById(
+      "current-" + currentPlayer
+    ).textContent = roundScore;
+  } else {
+    switchPlayers();
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  //add current round score to total score of current player
+  scores[currentPlayer] += roundScore;
+
+  //update the UI
+  document.getElementById("score-" + currentPlayer).textContent =
+    scores[currentPlayer];
+
+  // check if current player has won the game
+  if (scores[currentPlayer] >= 100) {
+      document.getElementById('name-'+ currentPlayer).textContent = 'Winner!';
+      resetCounters();
+  } else {
+    switchPlayers();
+  }
+});
+
+function switchPlayers() {
+  roundScore = 0;
+  document.getElementById("current-" + currentPlayer).textContent = roundScore;
+  currentPlayer === 0 ? (currentPlayer = 1) : (currentPlayer = 0);
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
+
+function resetCounters() {
+    scores = [0, 0];
+    roundScore = 0;
+    currentPlayer = 0; //player 0 starts the game
+
+    // don't show the dice at the beginning of the game
+    document.querySelector(".dice").style.display = "none";
+
+    for (i = 0; i < scores.length; i++) {
+        document.getElementById("score-" + i).textContent = "0";
+        document.getElementById("current-" + i).textContent = "0";
+      }
+  }
