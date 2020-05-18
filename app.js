447c7004aff4e43360164a998f7efef0168820bf
Houda Belefqih <houdabelefqih@gmail.com>
2020-05-18 20:19:53 +0000

    added logic and css of winning the game

--- app.js
+++ app.js
@@ -13,38 +13,47 @@ document.querySelector(".btn-new").addEventListener("click", resetCounters);
 
 // handle the rolling of the dice
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  var dice = Math.floor(Math.random() * 6) + 1;
 
-  // display the appropriate dice image
-  var diceDOM = document.querySelector(".dice");
-  diceDOM.style.display = "block";
-  diceDOM.src = "images/dice-" + dice + ".png";
+    if (!gameOver()){
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-  if (dice !== 1) {
-    roundScore += dice;
-    document.getElementById(
-      "current-" + currentPlayer
-    ).textContent = roundScore;
-  } else {
-    switchPlayers();
-  }
+        // display the appropriate dice image
+        var diceDOM = document.querySelector(".dice");
+        diceDOM.style.display = "block";
+        diceDOM.src = "images/dice-" + dice + ".png";
+
+        if (dice !== 1) {
+            roundScore += dice;
+            document.getElementById(
+            "current-" + currentPlayer
+            ).textContent = roundScore;
+        } else {
+            switchPlayers();
+        }
+}
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  //add current round score to total score of current player
-  scores[currentPlayer] += roundScore;
 
-  //update the UI
-  document.getElementById("score-" + currentPlayer).textContent =
-    scores[currentPlayer];
+    if (!gameOver()){
+        //add current round score to total score of current player
+        scores[currentPlayer] += roundScore;
 
-  // check if current player has won the game
-  if (scores[currentPlayer] >= 100) {
-      document.getElementById('name-'+ currentPlayer).textContent = 'Winner!';
-      resetCounters();
-  } else {
-    switchPlayers();
-  }
+        //update the UI
+        document.getElementById("score-" + currentPlayer).textContent =
+            scores[currentPlayer];
+
+        // check if current player has won the game
+        if (scores[currentPlayer] >= 100) {
+
+            document.getElementById('name-'+ currentPlayer).textContent = 'Winner!';
+            document.querySelector(".dice").style.display = "none";
+            document.querySelector(".player-"+currentPlayer+"-panel").classList.remove("active");
+            document.querySelector(".player-"+currentPlayer+"-panel").classList.add("winner");
+            } else {
+            switchPlayers();
+        }
+}
 });
 
 function switchPlayers() {
@@ -69,5 +78,13 @@ function resetCounters() {
     for (i = 0; i < scores.length; i++) {
         document.getElementById("score-" + i).textContent = "0";
         document.getElementById("current-" + i).textContent = "0";
+        player = i+1;
+        document.getElementById('name-'+ i).textContent = 'Player '+ player;
       }
   }
+
+function gameOver(){
+    if (scores[0]>=100 || scores[1] >= 100) return true;
+    else return false;
+    
+}
